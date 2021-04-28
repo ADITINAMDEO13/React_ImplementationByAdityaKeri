13ffa2d1b3eb027d1b1d5e7e3c54a9c648a91a54
Namdev <adnamdev@deloitte.com>
2021-04-28 23:40:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [display, setDisplay] = useState([
+    { name: "Iron Man", active: false, id: 1 },
+    { name: "Spider Man", active: false, id: 2 },
+    { name: "Deadpool", active: false, id: 3 },
+    { name: "Thor", active: false, id: 4 }]
   );
-}
+  
+  const [clas, setClas] = useState("cardactive")
+  
 
+  const changeBorder = (id) => {
+    const updatedData = display.map((item) => {
+      if (item.id === id) {
+        return {...item, active:!item.active}
+      }
+      else {
+        return {...item, active:false}
+      }
+      return item;
+    })
+    setDisplay(updatedData);
+  }
+
+
+  return (
+    < div className="displaydata">
+      {display.map((items) => 
+        <div className={items.active ? 'card' : 'cardactive'} onClick={()=>{changeBorder(items.id)}} > <h1>{items.name}</h1>
+        </div>
+      )}
+    </div>
+    );
+}
 export default App;
